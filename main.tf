25c2330ed69c72a275726014332ad43f63a9dcc8
Bhagwati0021 <bhagwati.bashyal@quantasis.com>
2025-11-12 15:20:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,13 @@
 terraform {
   required_version = ">= 1.6.0"
-
-  # Local backend just to keep state locally for now
   backend "local" {
-    path = "C:/terraform-results/terraform.tfstate"
+    path = "./terraform.tfstate"
   }
 }
 
-# A dummy local resource — purely for testing Terraform flow
 resource "local_file" "test_output" {
   content  = "Terraform test run successful!"
-  filename = "C:/terraform-results/test_output.txt"
+  filename = "${path.module}/test_output.txt"
 }
 
 output "file_path" {
